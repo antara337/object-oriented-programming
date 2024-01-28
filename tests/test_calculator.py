491a9f219175f4dc06219b870c08838ded05d39e
antai <ai337@njit.edu>
2024-01-28 17:58:08 -0500

    tried to fix error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,5 +7,3 @@ def test_calculator_operations():
     assert Calculator.divide(2, 2) == 1, "The Division Function Failed"
     assert Calculator.multiply(2, 2) == 4, "Multiplication Didn't work"
     assert Calculator.subtract(2, 2) == 0,  "subtraction didn't work"
-
-
