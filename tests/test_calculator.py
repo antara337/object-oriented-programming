cd4e8214071ea9373e934326f7d9dbfdfb26d56d
antai <ai337@njit.edu>
2024-01-28 18:00:01 -0500

    tried to fix error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,6 +3,7 @@ from app.calculator import Calculator
 
 
 def test_calculator_operations():
+    """Tests the calculator operations"""
     assert Calculator.add(2, 2) == 4, "The Addition Function Failed"
     assert Calculator.divide(2, 2) == 1, "The Division Function Failed"
     assert Calculator.multiply(2, 2) == 4, "Multiplication Didn't work"
